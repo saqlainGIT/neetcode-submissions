d71dcd4b2c785aeaa515499561b6580e5bccc5b9
Mohammed Saqlain <144561909+saqlainGIT@users.noreply.github.com>
2026-07-21 22:59:04 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.js
@@ -0,0 +1,42 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let left = 0;
+        let right = height.length - 1;
+
+        let leftMax = 0;
+        let rightMax = 0;
+
+        let water = 0;
+
+        while (left < right) {
+
+            // Process the side with the smaller boundary
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+}
